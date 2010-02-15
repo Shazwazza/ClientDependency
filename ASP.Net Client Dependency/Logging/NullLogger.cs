ec9e596a7f4d2f10263968dd9e9d6e7cebd36444
slace <none@none>
2010-02-15 10:54:41 +0000

    Missing logger file
    --HG--
    extra : convert_revision : svn%3A4ef1bfeb-0a34-4d02-9305-240e5b07a168%4055754

---
+++ ASP.Net Client Dependency/Logging/NullLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientDependency.Core.Logging
+{
+    internal class NullLogger : ILogger
+    {
+        #region ILogger Members
+
+        public void Debug(Action action)
+        {
+        }
+
+        public void Info(Action action)
+        {
+        }
+
+        public void Warn(Action action)
+        {
+        }
+
+        public void Error(Action action)
+        {
+        }
+
+        public void Fatal(Action action)
+        {
+        }
+
+        #endregion
+    }
+}
