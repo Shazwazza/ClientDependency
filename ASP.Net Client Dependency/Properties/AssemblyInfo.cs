5f3958de2a31c632340b9707bec10fe50ddcb23d
Shandem <none@none>
2010-03-16 23:29:13 +0000

    Updated assembly info
    --HG--
    extra : convert_revision : svn%3A4ef1bfeb-0a34-4d02-9305-240e5b07a168%4056662

--- ASP.Net Client Dependency/Properties/AssemblyInfo.cs
+++ ASP.Net Client Dependency/Properties/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClientDependency.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Script file and Css file management for web sites")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Farm Digital")]
 [assembly: AssemblyProduct("ClientDependency.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,19 +17,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("acb95044-dd5c-4d0a-8523-a6352470c424")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 //ADD RESOURCES
 [assembly: System.Web.UI.WebResource("ClientDependency.Core.Resources.LazyLoader.js", "text/javascript")]
--- ClientDependency.Web.Test/Properties/AssemblyInfo.cs
+++ ClientDependency.Web.Test/Properties/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClientDependency.Web.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Demo web site for testing and demonstrating ClientDependency usage")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Farm Digital")]
 [assembly: AssemblyProduct("ClientDependency.Web.Test")]
-[assembly: AssemblyCopyright("Copyright © The Farm Digital 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,15 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
