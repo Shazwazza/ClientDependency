a92a7b1c56cffd10c7c00e898fbbe8c982175756
mattbrailsford <none@none>
2010-08-18 19:18:42 +0000

    Added HtmlInclude control
    --HG--
    extra : convert_revision : svn%3A4ef1bfeb-0a34-4d02-9305-240e5b07a168%4065057

---
+++ ASP.Net Client Dependency/Controls/HtmlInclude.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+
+namespace ClientDependency.Core.Controls
+{
+    [ToolboxData("<{0}:HtmlInclude runat=\"server\"></{0}:HtmlInclude>")]
+    public class HtmlInclude : Literal
+    {
+        public const int DefaultPriority = 100;
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            var isNew = false;
+            var loader = ClientDependencyLoader.TryCreate(Page, out isNew);
+
+            RegisterIncludes(Text, loader);
+
+            Text = string.Empty;
+        }
+
+        private void RegisterIncludes(string innerHtml, ClientDependencyLoader loader)
+        {
+            var snippet = new HtmlDocument();
+            snippet.LoadHtml(innerHtml);
+
+            RegisterCssIncludes(snippet, loader);
+            RegisterJsIncludes(snippet, loader);
+        }
+
+        private void RegisterCssIncludes(HtmlDocument snippet, ClientDependencyLoader loader)
+        {
+            var nodes = snippet.DocumentNode.SelectNodes(@"//link[@type='text/css' and @href]");
+            if (nodes == null)
+                return;
+
+            var count = 0;
+            foreach (var att in nodes.Select(node => node.Attributes["href"]))
+            {
+                loader.RegisterDependency(DefaultPriority + count,
+                    att.Value,
+                    ClientDependencyType.Css);
+
+                count++;
+            }
+        }
+
+        private void RegisterJsIncludes(HtmlDocument snippet, ClientDependencyLoader loader)
+        {
+            var nodes = snippet.DocumentNode.SelectNodes(@"//script[@type='text/javascript' and @src]");
+            if(nodes == null)
+                return;
+
+            var count = 0;
+            foreach (var att in nodes.Select(node => node.Attributes["src"]))
+            {
+                loader.RegisterDependency(DefaultPriority + count,
+                    att.Value,
+                    ClientDependencyType.Javascript);
+
+                count++;
+            }
+        }
+    }
+}
