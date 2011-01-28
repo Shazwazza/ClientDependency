5f3c9109e3ced71306a5f20aeea574a0da898f1f
Shandem <none@none>
2011-01-28 10:17:23 +0000

    added rhino mocks and more extension methods
    --HG--
    extra : convert_revision : svn%3A4ef1bfeb-0a34-4d02-9305-240e5b07a168%4072606

---
+++ ClientDependency.Mvc/ControllerContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ClientDependency.Core.Mvc
+{
+    public static class ControllerContextExtensions
+    {
+        public static DependencyRenderer GetLoader(this ControllerContext cc)
+        {
+            bool isNew;
+            var instance = DependencyRenderer.TryCreate(cc.HttpContext, out isNew);
+            return instance;
+        }
+    }
+}
---
+++ ClientDependency.Mvc/HttpContextBaseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace ClientDependency.Core.Mvc
+{
+    public static class HttpContextBaseExtensions
+    {
+        public static DependencyRenderer GetLoader(this HttpContextBase http)
+        {
+            bool isNew;
+            var instance = DependencyRenderer.TryCreate(http, out isNew);
+            return instance;
+        }
+    }
+}
