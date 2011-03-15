806e83c0b93d5bc3eed326b3e7249a4180850102
Shandem <none@none>
2011-03-16 10:24:22 +1100

    Fixes:13162 Issue with JsMin and strange Js syntax, now it doesn't mess with spaces around + signs.

--- ASP.Net Client Dependency/CompositeFiles/JSMin.cs
+++ ASP.Net Client Dependency/CompositeFiles/JSMin.cs
@@ -338,7 +338,7 @@ namespace ClientDependency.Core.CompositeFiles
         bool isAlphanum(int c)
         {
             return ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') ||
-                (c >= 'A' && c <= 'Z') || c == '_' || c == '$' || c == '\\' ||
+                (c >= 'A' && c <= 'Z') || c == '_' || c == '$' || c == '\\' || c == '+' ||
                 c > 126);
         }
     }
--- ClientDependency.UnitTests/JsMinifyTest.cs
+++ ClientDependency.UnitTests/JsMinifyTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientDependency.Core.CompositeFiles;
 
 namespace ClientDependency.UnitTests
 {
@@ -10,9 +11,65 @@ namespace ClientDependency.UnitTests
     public class JsMinifyTest
     {
         [TestMethod]
-        public void Minify_Js()
+        public void JsMinify_Minify()
         {
-            Assert.Inconclusive();
+            //Arrange
+
+            var script =
+                @"var Messaging = {
+    GetMessage: function(callback) {
+        $.ajax({
+            type: ""POST"",
+            url: ""/Services/MessageService.asmx/HelloWorld"",
+            data: ""{}"",
+            contentType: ""application/json; charset=utf-8"",
+            dataType: ""json"",
+            success: function(msg) {
+                callback.apply(this, [msg.d]);
+            }
+        });
+    }
+    var blah = 1;
+    blah++;
+    blah = blah + 2;
+    var newBlah = ++blah;
+    newBlah += 234 +4;
+};";
+
+            var minifier = new JSMin();
+
+            //Act
+
+            var output = minifier.Minify(script);
+
+            Assert.AreEqual(
+                "\nvar Messaging={GetMessage:function(callback){$.ajax({type:\"POST\",url:\"/Services/MessageService.asmx/HelloWorld\",data:\"{}\",contentType:\"application/json; charset=utf-8\",dataType:\"json\",success:function(msg){callback.apply(this,[msg.d]);}});}\nvar blah=1;blah++;blah=blah + 2;var newBlah=++blah;newBlah +=234 +4;};",
+                output);
+        }
+
+        [TestMethod]
+        public void JsMinify_Minify_With_Unary_Operator()
+        {
+            //see: http://clientdependency.codeplex.com/workitem/13162
+
+            //Arrange
+
+            var script = 
+@"var c = {};
+var c.name = 0;
+var i = 1;
+c.name=i+ +new Date;
+alert(c.name);";
+
+            var minifier = new JSMin();
+            
+            //Act
+
+            var output = minifier.Minify(script);
+
+            //Assert
+
+            Assert.AreEqual("\nvar c={};var c.name=0;var i=1;c.name=i+ +new Date;alert(c.name);", output);
         }
     }
 }
