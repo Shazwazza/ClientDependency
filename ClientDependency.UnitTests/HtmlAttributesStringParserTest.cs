cf29d445263bac9c4a5c062be5bc8557e5eb0d07
shannon@ShandemVaio.Gateway <shannon@ShandemVaio.Gateway>
2012-01-24 21:44:00 +0700

    missing file

---
+++ ClientDependency.UnitTests/HtmlAttributesStringParserTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using ClientDependency.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ClientDependency.UnitTests
+{
+    
+    
+    [TestClass()]
+    public class HtmlAttributesStringParserTest
+    {
+        [TestMethod()]
+        public void Parse_Delimited_String_With_Comma()
+        {
+            const string attributes = "media:'print, projection'";
+            var destination = new Dictionary<string, string>();
+            
+            HtmlAttributesStringParser.ParseIntoDictionary(attributes, destination);
+            
+            Assert.AreEqual(1, destination.Count);
+            Assert.AreEqual("print, projection", destination.Last().Value);
+            Assert.AreEqual("media", destination.Last().Key);
+
+        }
+
+        [TestMethod()]
+        public void Parse_Normal_String()
+        {
+            const string attributes = "media:print";
+            var destination = new Dictionary<string, string>();
+
+            HtmlAttributesStringParser.ParseIntoDictionary(attributes, destination);
+
+            Assert.AreEqual(1, destination.Count);
+            Assert.AreEqual("print", destination.Last().Value);
+            Assert.AreEqual("media", destination.Last().Key);
+
+        }
+    }
+}
