dfa5385e0f176c242221b0ee5d7abad3adc46360
Shannon <sdeminick@gmail.com>
2014-04-30 13:52:01 +1000

    version updated

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,6 +7,6 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.7.2")]
-[assembly: AssemblyFileVersion("1.7.2")]
-[assembly: AssemblyInformationalVersion("1.7.2-beta")]
+[assembly: AssemblyVersion("1.8.0")]
+[assembly: AssemblyFileVersion("1.8.0")]
+[assembly: AssemblyInformationalVersion("1.8.0-beta")]
