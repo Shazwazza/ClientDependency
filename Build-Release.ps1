9a79849599e75eef11eeedd47d1bbc759de1b2f4
Shannon <sdeminick@gmail.com>
2015-11-26 17:55:13 +0100

    changes msbuild param

--- Build-Release.ps1
+++ Build-Release.ps1
@@ -13,7 +13,7 @@ $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 
 $SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
 
-$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+$MSBuild = "${Env:ProgramFiles(x88)}\MSBuild\14.0\Bin\msbuild.exe"
 
 # Make sure we don't have a release folder for this version already
 $BuildFolder = Join-Path -Path $SolutionRoot -ChildPath "build";
