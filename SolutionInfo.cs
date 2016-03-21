e053ec950fa1e13cf0d875227bce30f374b54e78
Shannon <sdeminick@gmail.com>
2016-03-21 14:18:27 +0100

    bumps version to 1.9.0-beta

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,11 +2,11 @@
 using System.Security;
 
 [assembly: AssemblyCompany("Shannon Deminick")]
-[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2015")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.8.4")]
-[assembly: AssemblyFileVersion("1.8.4")]
-[assembly: AssemblyInformationalVersion("1.8.4")]
+[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyInformationalVersion("1.9.0-beta")]
