d62321a7cdfe4bc044ddb37c14219e26acb52b7d
Shannon <sdeminick@gmail.com>
2017-10-13 13:26:13 +1100

    bumps version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,11 +2,11 @@
 using System.Security;
 
 [assembly: AssemblyCompany("Shannon Deminick")]
-[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2016")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.9.2")]
-[assembly: AssemblyFileVersion("1.9.2")]
-[assembly: AssemblyInformationalVersion("1.9.2")]
+[assembly: AssemblyVersion("1.9.3")]
+[assembly: AssemblyFileVersion("1.9.3")]
+[assembly: AssemblyInformationalVersion("1.9.3")]
